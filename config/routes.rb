a3be2ac127e40939a7a7c745dfcaa3f69ef7a4cb
sandwraith7495 <sandwraith7495@gmail.com>
2016-10-13 14:49:52 +0700

    BTVN tuan 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
-  get 'about'   =>'static_pages#about'
-  get 'help'    =>'static_pages#help'
-  get 'signup'  =>'users#new'
+  get 'about'      =>'static_pages#about'
+  get 'help'       =>'static_pages#help'
+  get 'signup'     =>'users#new'
+  get 'login'      =>'sessions#new'
+  post 'login'     =>'sessions#create'
+  delete 'logout'  =>'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
